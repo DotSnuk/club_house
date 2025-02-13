e1c79e37efde242c5d2474bc57c2c035349a620a
Hampus Farner <hampusfarner@outlook.com>
2025-02-13 08:55:55 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = require('./router/router');
+const cors = require('cors');
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+};
+require('dotenv').config();
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`app listening in on port ${PORT}`));
